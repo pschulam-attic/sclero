e78d39fb83fbb7ad55ebadfc42ac9afde25ea672
Peter Schulam <pschulam@gmail.com>
2013-09-19 12:51:17 -0400

    Add patient_coef function.

--- R/mixed-effects.r
+++ R/mixed-effects.r
@@ -8,3 +8,20 @@ fit_mixed_effects <- function(fixed, random, data) {
 
   return(lme.fit)
 }
+
+patient_coef <- function(lme.fit) {
+  cf <- coef(lme.fit)
+  new.names <- if (length(cf) == 1) {
+    c("intercept")
+  } else {
+    l <- length(cf)
+    c("intercept", names(cf)[2:l])
+  }
+
+  names(cf) <- new.names
+
+  cf[["patient.id"]] <- rownames(cf)
+  cf <- cf[, c("patient.id", new.names)]
+
+  return(cf)
+}
