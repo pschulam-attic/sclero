7c837111a5e3f0828b33795a72c01f3ca7a3865f
Peter Schulam <pschulam@gmail.com>
2013-09-19 12:41:31 -0400

    Fix bug in add_date_since function.

--- R/util.r
+++ R/util.r
@@ -1,12 +1,20 @@
 dev_subset <- function(data, patientsplit) {
   dev.patients <- unique(subset(patientsplit, dev == 1)$PtID)
-  subset(data, patient.id %in% dev.patients)
+  data <- subset(data, patient.id %in% dev.patients)
+
+  return(data)
 }
 
-add_date_since <- function(data, date.var, since.var, patient.data, baseline.var) {
+add_date_since <- function(data, date.var, since.var, patient.data,
+                           baseline.var, as.year = TRUE) {
   patient.baseline <- structure(as.Date(patient.data[[baseline.var]]),
                                 names = patient.data$patient.id)
 
-  data[[since.var]] <- with(data, as.Date(date.var) - patient.baseline[patient.id])
-  data[[since.var]] <- as.numeric(data[[since.var]])
+  dates <- as.Date(data[[date.var]])
+  data[[since.var]] <- dates - patient.baseline[data$patient.id]
+
+  div <- if (as.year) 365 else 1
+  data[[since.var]] <- as.numeric(data[[since.var]]) / div
+
+  return(data)
 }
