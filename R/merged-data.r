36c382c1b8e8e09a981e9a76c894e6aa3ee11e13
Peter Schulam <pschulam@gmail.com>
2013-10-04 17:13:27 -0400

    Whoops, actually refer to data frames.

--- R/merged-data.r
+++ R/merged-data.r
@@ -6,8 +6,8 @@
 sclero_merged <- function() {
   data(list=c("patient", "clinic", "pft", "sero"))
   
-  df <- merge("clinic", "pft", by=c("patient.id", "date"), all=TRUE)
-  df <- merge(df, "sero", by=c("patient.id", "date"), all=TRUE)
+  df <- merge(clinic, pft, by=c("patient.id", "date"), all=TRUE)
+  df <- merge(df, sero, by=c("patient.id", "date"), all=TRUE)
   df <- merge(patient, df, by="patient.id")
 
   return(df)
