7ebd43f7ce93b2a88b216714059aadbd14587a06
Peter Schulam <pschulam@gmail.com>
2013-10-04 17:08:09 -0400

    Add sclero_merged function.

---
+++ R/merged-data.r
@@ -0,0 +1,14 @@
+#' Load the merged scleroderma data.
+#'
+#' @export
+#' @author Peter Schulam <pschulam@gmail.com>
+#' 
+sclero_merged <- function() {
+  data(list=c("patient", "clinic", "pft", "sero"))
+  
+  df <- merge("clinic", "pft", by=c("patient.id", "date"), all=TRUE)
+  df <- merge(df, "sero", by=c("patient.id", "date"), all=TRUE)
+  df <- merge(patient, df, by="patient.id")
+
+  return(df)
+}
